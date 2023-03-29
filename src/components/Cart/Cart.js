9dc8c43d2d87017a101e6db5da6143e5e4052255
Yashraj Shukla <yashrajshukla48@gmail.com>
2023-03-29 10:41:06 +0530

    Worked on Cart.js and its UI

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,28 @@
+import classes from "./Cart.module.css";
+
+const Cart = (props) => {
+  const cartItems = (
+    <ul className={classes["cart-items"]}>
+      {[{ id: "c1", name: "Veg Kotha", amount: 2, price: 12.99 }].map(
+        (item) => (
+          <li>{item.name}</li>
+        )
+      )}
+    </ul>
+  );
+
+  return (
+    <div>
+      {cartItems}
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span> 350 </span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes["button--alt"]}>Close</button>
+        <button className={classes.button}>Order</button>
+      </div>
+    </div>
+  );
+};
+export default Cart;
