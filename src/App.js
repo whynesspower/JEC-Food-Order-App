ce73e6d433a62edf45efea72f7c55f06b1f61a6c
Yashraj Shukla <yashrajshukla48@gmail.com>
2023-03-30 01:01:37 +0530

    added cart and modal compenent

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import { useState } from "react";
+import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 function App() {
+  const [cartIsShow, setCartIsShown] = useState(false);
   return (
     <>
+      <Cart />
       <Header></Header>
       <main>
         <Meals />
--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,3 +1,4 @@
+import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
 
 const Cart = (props) => {
@@ -12,17 +13,17 @@ const Cart = (props) => {
   );
 
   return (
-    <div>
+    <Modal>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span> 350 </span>
+        <span> ₹ 350 </span>
       </div>
       <div className={classes.actions}>
         <button className={classes["button--alt"]}>Close</button>
         <button className={classes.button}>Order</button>
       </div>
-    </div>
+    </Modal>
   );
 };
 export default Cart;
