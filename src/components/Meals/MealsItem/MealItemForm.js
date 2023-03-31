3bc856cf5a059a8cbf6c3c8fcc05697da6b92983
Yashraj Shukla <yashrajshukla48@gmail.com>
2023-03-31 11:44:13 +0530

    preventDefault submit in MealItemForm

--- src/components/Meals/MealsItem/MealItemForm.js
+++ src/components/Meals/MealsItem/MealItemForm.js
@@ -1,8 +1,12 @@
 import Input from "../../UI/Input";
 import classes from "./MealItemForm.module.css";
+
 const MealItemForm = (props) => {
+  const submitHandler = (event) => {
+    event.preventDefault();
+  };
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <Input
         label="Amount"
         input={{
